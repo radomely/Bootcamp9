e82c7c8a6a94afca0c9de1f7ff78be8ab01fc295
Andrii <radomely@gmail.com>
2018-12-19 18:36:03 +0200

    added JavaScript day 1

---
+++ JavaScript/JS_1/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// let firstNumber;
+// const secondNumber = 15;
+// console.log(firstNumber);
+// firstNumber = 5;
+// let a;
+// a=null;
+// console.log(typeof a);
+// let b;
+// console.log(typeof a);
+// a=0.3;
+// b=a+0.5;
+// console.log(typeof b);
+// a="dfsfs 53";
+// // alert("Авторизаци прошла удачно!");
+// console.log(typeof a);
+
+// const isComing = confirm('Введите ваш возраст');
+// console.log(isComing);
+
+// const hotelName = prompt('Введите ваше имя:');
+// console.log(Number(hotelName));
+
+// console.log("0.5 % 0.5 = " + 0.5 % 0.5);
+// console.log("a""а"); 
+// console.log(0.1==0.1);
+// console.log(0.1===0.1);
+// console.log(0.1 != 0.2);
+
+// console.log(0.1=="0.1");
+// console.log(0.1 ===0.1);
+// console.log(0.1 ==="0.1");
+// console.log("aaa" != "aaA");
+// let a=[1,2,3];
+// let b=a;
+// console.log(a==b);
+
+// console.log([]===[]);
+
+let px = 10;
+let promNam = 15 +"";
+px = String(px);
+let str = '5' + SVement;
+
+console.log(`Вы заказали ${promNam} и это будет стоить ${25+2}`);
+// console.log(Number(promNam));
+// console.log(Number.parseInt(promNam));
+// console.log(Number.parseFloat(promNam));
+
+ 
